0fbbdbb202045717b1fc037e6978948b301ef040
unknown <th7kingdom@gmail.com>
2021-09-11 00:33:13 +0200

    Start a Working Express Server

--- myApp.js
+++ myApp.js
@@ -3,6 +3,9 @@ var app = express();
 var bGround = require('fcc-express-bground');
 bGround.log("Hello World");
 console.log("Hello World");
+app.get('/',(req,res)=>{
+    res.send("Hello Express")
+})
 
 
 
