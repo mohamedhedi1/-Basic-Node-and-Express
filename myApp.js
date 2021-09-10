059c2d1c315acfb2da4d1c45d6eb72c0d780f7c9
unknown <th7kingdom@gmail.com>
2021-09-11 01:14:33 +0200

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -3,7 +3,10 @@ var app = express();
 var bGround = require('fcc-express-bground');
 bGround.log("Hello World");
 console.log("Hello World");
+
+app.use("/public",express.static(__dirname + "/public"))
 app.get('/',(req,res)=>{
+   
     res.sendFile(__dirname+"/views/index.html")
 })
 
