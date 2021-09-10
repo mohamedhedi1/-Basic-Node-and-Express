2fa84f394d97511efc374c3da66ea9fbe5639b0b
unknown <th7kingdom@gmail.com>
2021-09-11 00:17:42 +0200

    Meet the Node console fixed

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var bGround = require('fcc-express-bground');
-bGround.log("Hello World")
+bGround.log("Hello World");
+console.log("Hello World");
 
 
 
